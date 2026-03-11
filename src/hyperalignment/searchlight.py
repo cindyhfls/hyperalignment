d6ee1f17cf1b452dd33837a4c597dc999dfc8c72
cindyhfls <cindyhfls@gmail.com>
2026-03-11 15:23:57 -0400

    bug fix for searchlight hyperalignment parallel loop

--- src/hyperalignment/searchlight.py
+++ src/hyperalignment/searchlight.py
@@ -135,11 +135,11 @@ def searchlight_hyperalignment(
                 for sl_X, sl_Y in zip(sls, sls_Y)
             )
         if weights is not None:
-            for t, w, sl in zip(local_xfms, weights, sls):
+            for t, sl_X, sl_Y, w in zip(local_xfms, sls, sls_Y, weights):
                 mat[np.ix_(sl_X, sl_Y)] += t * w[np.newaxis]
         else:
             warnings.warn('Legacy, do not use this, use searchlight_weights(dists=None) to get uniform weights as input to this function instead')
-            for t, sl in zip(local_xfms, sls):
+            for t, sl_X, sl_Y in zip(local_xfms, sls, sls_Y):
                 mat[np.ix_(sl_X, sl_Y)] += t
     return mat
 
